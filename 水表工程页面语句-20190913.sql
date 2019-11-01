d037b14be05cb56080e0201b816bb01170d7888c
yjw <yjw@softcloud.com>
2019-11-01 14:52:51 +0800

    修正

--- 水表工程页面语句-20190913.sql
+++ 水表工程页面语句-20190913.sql
@@ -233,7 +233,7 @@ SELECT SUM(t.year_flow) year_flow, --本年用水应去掉高区3个子级水表
  WHERE t.node_id = c.node_id
 
 --当年当月用水分析
-SELECT t.node_id, p.month_plan, t.month_flow,
+SELECT t.node_id, n.name node_name, p.month_plan, t.month_flow,
        CASE WHEN t.month_flow > p.month_plan THEN 1 ELSE 0 END is_over
   FROM (SELECT b.node_id,
                SUM(a.water_flow)-NVL(SUM(d.accu_flow),0)+NVL(SUM(d.month_flow),0) month_flow
@@ -241,11 +241,9 @@ SELECT t.node_id, p.month_plan, t.month_flow,
                        row_number() OVER (PARTITION BY ip_address ORDER BY collect_time DESC) rn
                   FROM ht_water_collection
                  WHERE collect_time >= to_char(TRUNC(SYSDATE), 'yyyy-mm-dd')) a,
-               (SELECT ip_addr,
+               (SELECT ip_addr, accu_flow,
                        (CASE WHEN substr(acct_day, 1, 6)=to_char(SYSDATE, 'yyyymm')
-                            THEN month_flow else 0 END)
-                       (CASE WHEN substr(acct_day, 1, 6)=to_char(SYSDATE, 'yyyymm')
-                            THEN accu_flow else 0 END) accu_flow
+                             THEN month_flow else 0 END) month_flow
                   FROM ht_water_stat_day
                  WHERE acct_day = to_char(SYSDATE - 1, 'yyyymmdd')) d,
                (SELECT node_id, ip_addr FROM a_device) b
@@ -253,21 +251,23 @@ SELECT t.node_id, p.month_plan, t.month_flow,
          GROUP BY b.node_id) t,
        (SELECT node_id,
                CASE to_char(SYSDATE, 'mm')
-                 WHEN '01' THEN jan_plan
-                 WHEN '02' THEN feb_plan
-                 WHEN '03' THEN mar_plan
-                 WHEN '04' THEN apr_plan
-                 WHEN '05' THEN may_plan
-                 WHEN '06' THEN june_plan
-                 WHEN '07' THEN july_plan
-                 WHEN '08' THEN aug_plan
-                 WHEN '09' THEN sept_plan
-                 WHEN '10' THEN oct_plan
-                 WHEN '11' THEN nov_plan
-                 WHEN '12' THEN dec_plan
+                WHEN '01' THEN jan_plan
+                WHEN '02' THEN feb_plan
+                WHEN '03' THEN mar_plan
+                WHEN '04' THEN apr_plan
+                WHEN '05' THEN may_plan
+                WHEN '06' THEN june_plan
+                WHEN '07' THEN july_plan
+                WHEN '08' THEN aug_plan
+                WHEN '09' THEN sept_plan
+                WHEN '10' THEN oct_plan
+                WHEN '11' THEN nov_plan
+                WHEN '12' THEN dec_plan
                END month_plan
-          FROM a_water_plan WHERE YEAR = to_char(SYSDATE, 'yyyy')) p
- WHERE t.node_id = p.node_id(+);
+          FROM a_water_plan WHERE YEAR = to_char(SYSDATE, 'yyyy')) p,
+       a_node n
+ WHERE t.node_id = p.node_id(+) AND t.node_id = n.id AND p.node_id = n.id
+ ORDER BY n.weight;
 
 --漏水逻辑（注意有2参数，由外部输入）
 INSERT INTO a_warn_msg(
