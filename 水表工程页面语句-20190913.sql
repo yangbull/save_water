ce03277f94a8fd49159d6961325ab963c4bfadf7
yjw <yjw@softcloud.com>
2019-11-02 14:29:41 +0800

    修复跨月跨年问题

--- 水表工程页面语句-20190913.sql
+++ 水表工程页面语句-20190913.sql
@@ -104,10 +104,12 @@ INSERT INTO ht_water_stat_day(
   h23_flow
 ) SELECT b.node_id, a.ip_address,
          SUM(CASE WHEN acct_type IN (0, -1) AND a.rn = 1 THEN water_flow END) daily_flow,
-         SUM(CASE WHEN acct_type IN (0, -3) AND a.rn = 1 THEN water_flow END) month_flow,
-         SUM(CASE WHEN acct_type IN (0, -6) AND a.rn = 1 THEN water_flow END) year_flow,
+         SUM(CASE WHEN to_char(SYSDATE-1, 'dd') = '01' AND acct_type IN (0, -1) AND a.rn = 1 THEN water_flow
+                  WHEN acct_type IN (0, -3) AND a.rn = 1 THEN water_flow END) month_flow,
+         SUM(CASE WHEN to_char(SYSDATE-1, 'mmdd') = '0101' AND acct_type IN (0, -1) AND a.rn = 1 THEN water_flow
+                  WHEN acct_type IN (0, -6) AND a.rn = 1 THEN water_flow END) year_flow,
          SUM(CASE WHEN acct_type = 0 AND a.rn = 1 THEN water_flow END) accu_flow,
-         to_char(SYSDATE-1,'yyyymmdd') acct_day,
+         to_char(SYSDATE-1, 'yyyymmdd') acct_day,
          NVL(SUM(CASE WHEN acct_type = 0 AND collect_hour = '00' THEN water_flow END),0)+
          NVL(SUM(CASE WHEN acct_type = -1 AND collect_hour = '23' THEN water_flow END),0) h00_flow,
          NVL(SUM(CASE WHEN acct_type = 0 AND collect_hour = '01' THEN water_flow END),0)-
@@ -157,14 +159,14 @@ INSERT INTO ht_water_stat_day(
          NVL(SUM(CASE WHEN acct_type = 0 AND collect_hour = '23' THEN water_flow END),0)-
          NVL(SUM(CASE WHEN acct_type = 0 AND collect_hour = '22' THEN water_flow END),0) h23_flow
     FROM (SELECT ip_address, SUBSTR(collect_time, 1, 10) collect_day, SUBSTR(collect_time, 12, 2) collect_hour,
-                 DECODE(SUBSTR(collect_time,1,10),to_char(SYSDATE-1,'yyyy-mm-dd'),positive_flow_sum,-positive_flow_sum) water_flow,
+                 DECODE(SUBSTR(collect_time, 1, 10),to_char(SYSDATE-1,'yyyy-mm-dd'),positive_flow_sum,-positive_flow_sum) water_flow,
                  CASE WHEN SUBSTR(collect_time, 1, 10) = to_char(SYSDATE-1,'yyyy-mm-dd') THEN 0
                       WHEN SUBSTR(collect_time, 1, 10) = to_char(SYSDATE-2,'yyyy-mm-dd') THEN -1
                       WHEN SUBSTR(collect_time, 1, 7) = to_char(add_months(SYSDATE-1,-1),'yyyy-mm') THEN -3
                       WHEN SUBSTR(collect_time, 1, 4) = to_char(SYSDATE-1,'yyyy')-1 THEN -6
                  END acct_type, --账期类型
                  row_number() OVER(PARTITION BY ip_address, SUBSTR(collect_time, 1, 10) ORDER BY collect_time DESC) rn,
-				 row_number() OVER(PARTITION BY ip_address, SUBSTR(collect_time, 1, 13) ORDER BY collect_time DESC) rn1
+                 row_number() OVER(PARTITION BY ip_address, SUBSTR(collect_time, 1, 13) ORDER BY collect_time DESC) rn1
             FROM ht_water_collection
            WHERE collect_time >= to_char(TRUNC(SYSDATE)-1,'yyyy-mm-dd hh24:mi:ss')
              AND collect_time < to_char(TRUNC(SYSDATE),'yyyy-mm-dd hh24:mi:ss')   --基准日
